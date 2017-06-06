f29dc6cb44fb8f2d64b1c3d8a9fe931fc34fd841
Andrew Veenstra <aveenstra@berkeley.edu>
2017-06-05 23:38:29 -0700

    updated smtp settings with Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   end
 
   # Mailer for Devise FIXME (address, port must be fixed)
-  config.action_mailer.default_url_options = { :host => 'smtp.mailgun.org' }
+  config.action_mailer.default_url_options = { :host => 'innovate-berkeley.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['username'],
