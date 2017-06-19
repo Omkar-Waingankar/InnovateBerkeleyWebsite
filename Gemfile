68aa7547607fe064ac16f87d80ee73e14b869b86
Andrew Veenstra <aveenstra@berkeley.edu>
2017-06-18 19:30:44 -0700

    icons

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'rails_12factor', group: :production
 # Use for gmail mailer
 gem 'figaro'
 # Use for Skrollr scrolling functionality
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,10 @@ Rails.application.configure do
   :enable_starttls_auto => true  
   }
 
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
